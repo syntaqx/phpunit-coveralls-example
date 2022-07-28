5a73576d2b14383c77b5f31d620251eb91c38834
Chase Pierce <syntaqx@gmail.com>
2022-07-28 10:46:41 -0600

    adding comments

--- src/Example.php
+++ src/Example.php
@@ -4,6 +4,11 @@ namespace Example;
 
 class Example
 {
+    /**
+     * The simplest of functions.
+     *
+     * @return bool
+     */
     public function simpleReturnTrue()
     {
         return true;
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,6 +7,11 @@ use PHPUnit\Framework\TestCase;
 
 final class ExampleTest extends TestCase
 {
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
     public function testSimpleReturnTrue(): void
     {
         $obj = new Example();
